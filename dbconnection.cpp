0f51c673dd18c42677d894a6bd73041bf98c66db
napieralskaa <agata.napieralska1205@gmail.com>
2020-06-15 21:15:57 +0200

    DB is now in same folder as EXE
    It has to be there but it is not something that moves the DB there by itself cause I'm too stupid for that

--- dbconnection.cpp
+++ dbconnection.cpp
@@ -1,14 +1,19 @@
 #include "dbconnection.h"
+#include <QStandardPaths>
+#include <QCoreApplication>
+
 
 
 void DBConnection::connClose() {
     mydb.close();
     mydb.removeDatabase(QSqlDatabase::defaultConnection);
 }
-
 bool DBConnection::connOpen() {
+    //QString path=QStandardPaths::findExecutable("LibraryPro.exe");
+    QString path=QCoreApplication::applicationDirPath()+"/prodigy.db";
+    qDebug()<<path;
     mydb = QSqlDatabase::addDatabase("QSQLITE");
-    mydb.setDatabaseName("./prodigy.db");
+    mydb.setDatabaseName(path);
 
     if (!mydb.open())
     {
